953ef140b7088d9873505de1400329340094b243
hamoudaattia1 <hamouda.attia@esprit.tn>
2024-10-14 13:45:03 +0100

    jenkinsfile

--- Backend/src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
+++ Backend/src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ProductServiceImplTest {
+class SupplierServiceImplTest {
 
     @Mock
     private ProductRepository productRepository;
