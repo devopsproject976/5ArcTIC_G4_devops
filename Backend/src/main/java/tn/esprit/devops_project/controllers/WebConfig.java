82b39f78b103e37c0c006fc42f8e9dbc5004caf4
amenallahsd121 <amensmida121@gmail.com>
2024-11-04 17:41:50 +0100

    Bug

--- Backend/src/main/java/tn/esprit/devops_project/controllers/WebConfig.java
+++ Backend/src/main/java/tn/esprit/devops_project/controllers/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.202.128:4200") // Update this to the correct Angular app URL
+                .allowedOrigins("http://192.168.202.128:4200") // Update 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
